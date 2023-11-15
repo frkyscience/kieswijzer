b2fd06aff22eb6e3df21b1b59126b6bf5af193a5
Jesse_Bogaart <jesseb99@live.nl>
2023-11-15 11:39:24 +0100

    javascript uiot hmtl naar file gedaan

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
-const parties = [
-  { name: "Bij1", points: 0 },
-  { name: "Pvv", points: 0 },
-  { name: "fvd", points: 0 },
-  { name: "vvd", points: 0 },
-];
+let currentQuestion = 1;
 
-const questions = [
-  { q: "woke en lhbt-propaganda tegengaan", a: [-1, 0, 1, 2] },
-  { q: "economie & ondernemingen", a: [-1, 0, 1, 2] },
-  { q: "anti-racisme en dekolonisatie", a: [-1, 0, 1, 2] },
-  { q: "financiën omlaag voor burgers", a: [-1, 0, 1, 2] },
-];
+function nextQuestion() {
+  const currentSection = document.getElementById(`question${currentQuestion}`);
+  const form = document.getElementById('politicalTestForm');
 
-let currentQuestionIndex = 0;
+  const selectedOption = form.querySelector(`input[name="q${currentQuestion}"]:checked`);
+  if (!selectedOption) {
+    alert('Please select an option before proceeding.');
+    return;
+  }
 
-function displayQuestion() {
-  const { q, a } = questions[currentQuestionIndex];
-  document.getElementById("ques").textContent = q;
-  document.getElementById("opt").innerHTML = a.map((option, index) => `<input type="radio" name="options" value="${index + 1}" id="option${index + 1}"><label for="option${index + 1}">${option}</label><br>`).join('');
-}
+  currentSection.style.display = 'none';
+  currentQuestion++;
 
-function checkAns() {
-  const selectedOption = document.querySelector('input[name="options"]:checked');
-  if (selectedOption) {
-    const points = parseInt(selectedOption.value) - 1;
-    parties.forEach((party, index) => (party.points += points * questions[currentQuestionIndex].a[index]));
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-      displayQuestion();
-    } else {
-      showResult();
-    }
+  if (currentQuestion <= 4) {
+    const nextSection = document.getElementById(`question${currentQuestion}`);
+    nextSection.style.display = 'block';
   } else {
-    alert("Geef een optie voordat je door kan ");
+    calculateResult();
   }
 }
 
-function showResult() {
-  const winningParty = parties.reduce((max, party) => (party.points > max.points ? party : max), parties[0]);
-  document.getElementById("quiz-container").style.display = "none";
-  document.getElementById("result-container").style.display = "block";
-  document.getElementById("score").textContent = `Your recommended party is: ${winningParty.name}`;
+function calculateResult() {
+  const q1 = parseInt(document.querySelector('input[name="q1"]:checked').value);
+  const q2 = parseInt(document.querySelector('input[name="q2"]:checked').value);
+  const q3 = parseInt(document.querySelector('input[name="q3"]:checked').value);
+  const q4 = parseInt(document.querySelector('input[name="q4"]:checked').value);
+
+  if (q1 && q2 && q3 && q4) {
+    const totalScore = q1 + q2 + q3 + q4;
+
+    let result;
+    if (totalScore >= 30) {
+      result = 'Je bent sterk geassocieerd met de VVD.';
+    } else if (totalScore >= 20) {
+      result = 'Je bent sterk geassocieerd met de PvdA.';
+    } else if (totalScore >= 10) {
+      result = 'Je bent sterk geassocieerd met de PVV.';
+    } else {
+      result = 'Je bent sterk geassocieerd met GroenLinks.';
+    }
+
+    console.log(`Total Score: ${totalScore}`);
+    showResult(result);
+  } else {
+    showResult('Selecteer a.u.b. een antwoord voor elke vraag.');
+  }
 }
 
-displayQuestion();
+function showResult(result) {
+  document.getElementById('result').innerHTML = result;
+}
